2556479cdd490cf4b63103c75fd1b29b96e7ba55
Mariel907 <marielhubahib90@gmail.com>
2024-09-24 19:16:28 -0700

    Add files via upload

---
+++ NumSibling/NumSibling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Project_Program_104
+{
+    internal class MethodParameters_101
+    {
+        public static void Main(string[] args)
+        {
+            string name;
+            Console.Write("Enter your Name: ");
+            name = Console.ReadLine();
+            Console.Write("How many is/are your sibling/siblings?: ");
+            numSibling(int.Parse(Console.ReadLine()));
+        }
+        static int numSibling(int n)
+        {
+            string[] sib = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Enter the name of sibling {i + 1}: ");
+                sib[i] = Console.ReadLine();
+            }
+
+            if (n == 0)
+            {
+                Console.WriteLine("ohh, you are the only child. Ok");
+            }
+            return n;
+        }
+    }
+}
---
+++ Polymorphism/PeppaPig.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Poly
+{
+    class Polymorphism
+    {
+        public static void Main(string[] args)
+        {
+            string name1;
+
+            DaddyPig a = new DaddyPig();
+            a.dadPig();
+
+            Console.WriteLine();
+            Console.Write("Enter your name : ");
+            name1 = Console.ReadLine();
+            Console.WriteLine();
+
+            DaddyPig b = new PeppaPig(name1);
+            b.dadPig();
+            DaddyPig c = new MommyPig();
+            c.dadPig();
+        }
+    }
+    class DaddyPig
+    {
+        virtual public void dadPig()
+        {
+            Console.WriteLine("Hi I'm daddy pig");
+        }
+    }
+    class PeppaPig : DaddyPig
+    {
+        public string name { get; set; }
+        public PeppaPig(string Name)
+        {
+            name = Name;
+        }
+        override public void dadPig()
+        {
+            Console.WriteLine("I'm Peppa Pig and this is" + " " + name);
+        }
+    }
+    class MommyPig : DaddyPig
+    {
+        override public void dadPig()
+        {
+            Console.WriteLine("and I'm mommy pig.");
+        }
+    }
+}
