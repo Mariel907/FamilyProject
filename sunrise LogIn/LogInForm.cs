0c4cbed3c1499f8590ffcdaa63121f8c8f4aea35
Mariel907 <marielhubahib90@gmail.com>
2024-10-02 18:07:48 -0700

    Update LogInForm.cs

--- sunrise LogIn/LogInForm.cs
+++ sunrise LogIn/LogInForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,23 +18,40 @@ namespace sunrise_LogIn
         }
         private void BtnEnter(object sender, EventArgs e)
         {
-            MessageBox.Show($" Welcome Maam/Sir {textDisplay.Text}, you have been booked. ");
 
             if (textDisplay.Text == "Mariel" && textPin_Code.Text == "1234")
             {
+                MessageBox.Show($" Welcome Maam/Sir {textDisplay.Text}, you have been booked. ");
                 MainForm mainform = new MainForm();
                 mainform.UserName = "Mariel";
                 mainform.Show();
 
                 this.Hide();
             }
-            else
+            else if (textDisplay.Text == string.Empty || textPin_Code.Text == string.Empty)
             {
-                MessageBox.Show("Invalid UserName or PIN", "LogIn Failed",MessageBoxButtons.OK ,MessageBoxIcon.Error);
+                MessageBox.Show("UserName or PIN is empty", "LogIn Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 textDisplay.Clear();
                 textPin_Code.Clear();
                 textDisplay.Focus();
             }
+            else 
+            {
+                MessageBox.Show("Invalid UserName or PIN", "LogIn Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textDisplay.Clear();
+                textPin_Code.Clear();
+                textDisplay.Focus();
+            }
+        }
+        private void BtnSign_Up_Click(object sender, EventArgs e)
+        {
+            BookIn bookin = new BookIn();
+            bookin.Show();
+            this.Hide();
+        }
+         private void OnClose(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
